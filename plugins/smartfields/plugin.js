9d5044d8acc68556c811ad15e01d2d86daff7d6f
Seyar Chapuh <59485918+seyar-parleypro@users.noreply.github.com>
2020-01-30 22:27:49 +0300

    chore(par-9817): Need send smartField on template save (#15)
    * chore(par-9817): Need send smartField on template save
    
    * fix: initial load

--- plugins/smartfields/plugin.js
+++ plugins/smartfields/plugin.js
@@ -1,48 +1,49 @@
-( function() {
-	CKEDITOR.plugins.add( 'smartfields', {
-		lang: 'en,ru', // %REMOVE_LINE_CORE%
+(function () {
+  CKEDITOR.plugins.add('smartfields', {
+    lang: 'en,ru', // %REMOVE_LINE_CORE%
     hidpi: true, // %REMOVE_LINE_CORE%
-		onLoad: function() {
-			// Register styles for placeholder widget frame.
-			CKEDITOR.addCss( '.cke_placeholder{background-color:#ff0}' );
-		},
+    onLoad: function () {
+      // Register styles for placeholder widget frame.
+      CKEDITOR.addCss('.cke_placeholder{background-color:#ff0}');
+    },
 
-		init: function( editor ) {
-      editor.ui.addRichCombo( 'smartfields', {
+    init: function (editor) {
+      editor.ui.addRichCombo('smartfields', {
         label: 'Smart field',
         title: 'Add a smart field',
         toolbar: 'insert,5',
 
         panel: {
-          css: [ CKEDITOR.skin.getPath( 'editor' ) ].concat( editor.config.contentsCss ),
+          css: [CKEDITOR.skin.getPath('editor')].concat(editor.config.contentsCss),
           multiSelect: false
         },
 
-        init: function() {
-          this.startGroup( 'Smart Fields' );
+        init: function () {
+          this.startGroup('Smart Fields');
 
           var self = this;
           // this.startGroup( 'My Dropdown Group #1' );
           var smartFields = editor.config.smartFields || [];
-          smartFields.forEach(function(smartField) {
-            self.add( smartField.value, smartField.label );
+
+          smartFields.forEach(function (smartField) {
+            self.add(smartField.value, smartField.label);
           })
         },
 
-        onClick: function( id ) {
+        onClick: function (id) {
           editor.focus();
-          editor.fire( 'saveSnapshot' );
+          editor.fire('saveSnapshot');
 
           var fragment = editor.getSelection().getRanges()[0].extractContents();
-					var container = CKEDITOR.dom.element.createFromHtml('<span class="cke_placeholder" ' +
-						'>[[' + id + ']]</span>', editor.document);
+          var container = CKEDITOR.dom.element.createFromHtml('<span class="cke_placeholder smart_field" ' +
+            '>[[' + id + ']]</span>', editor.document);
 
-					fragment.appendTo(container);
-					editor.insertElement(container);
-					editor.widgets.initOn( container, 'placeholder' );
+          fragment.appendTo(container);
+          editor.insertElement(container);
+          editor.widgets.initOn(container, 'placeholder');
         }
-    } );
-		}
-	} );
+      });
+    }
+  });
 
-} )();
+})();
